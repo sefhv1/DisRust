c90be0addfdaa424d5cfdadb80becaca1338f15b
sefhv1 <167860682+sefhv1@users.noreply.github.com>
2024-10-07 19:48:51 +0900

    created

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the download button element
+const downloadButton = document.getElementById('download-button');
+
+// Add a click event listener to the button
+downloadButton.addEventListener('click', () => {
+    // You can add your download logic here
+    console.log('Download button clicked!');
+});
